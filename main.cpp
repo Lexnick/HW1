1e3ebf9fb5710f545a01e527e4d334754f57a6d1
Lexnick <70635054+Lexnick@users.noreply.github.com>
2021-04-08 16:38:46 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,10 +9,6 @@ struct block {
     unsigned char Data[8];
 };
 
-struct Encrypt {
-    block This;
-    Encrypt *next = nullptr;
-};
 
 void operator^(block &Text, const block &Gamma) {
     for (int i = 0; i < 8; i++) {
@@ -24,9 +20,9 @@ void operator>>(block &Block, const int &Number) {
     unsigned char Shift = 0;
     for (int i = 0; i < 8; i++) {
         unsigned char SavedData = Block.Data[i];
-        Block.Data[i] = Block.Data[i] >> Number;
-        Block.Data[i] += Shift;
-        Shift = (SavedData - (Block.Data[i] << Number)) << (8 - Number);
+        Block.Data[i] >>= Number;
+        Block.Data[i] |= Shift;
+        Shift = (SavedData ^ (Block.Data[i] << Number)) << (8 - Number);
     }
     Block.Data[0] += Shift;
 }
@@ -35,15 +31,15 @@ void operator<<(block &Block, const int &Number) {
     unsigned char Shift = 0;
     for (int i = 7; i > -1; i--) {
         unsigned char SavedData = Block.Data[i];
-        Block.Data[i] = Block.Data[i] << Number;
-        Block.Data[i] += Shift;
-        Shift = (SavedData - (Block.Data[i] >> Number)) >> (8 - Number);
+        Block.Data[i] <<= Number;
+        Block.Data[i] |= Shift;
+        Shift = (SavedData ^ (Block.Data[i] >> Number)) >> (8 - Number);
     }
     Block.Data[7] += Shift;
 }
 
-void Decrypting(std::string &Text, const std::string &Key, const std::string &FileOut) {
-    Text=' '+Text;
+void Encrypting(std::string &Text, const std::string &Key, const std::string &FileOut) {
+    Text = "  " + Text;
     float A = Text.size() / 8.0;
     float B = static_cast<float>(Text.size() / 8);
     unsigned int Amount = 0;
@@ -55,15 +51,16 @@ void Decrypting(std::string &Text, const std::string &Key, const std::string &Fi
 
 
     block *TextInBlocks = new block[Amount];
-    TextInBlocks[0].Data[0]=Text.size()-1;
+    TextInBlocks[0].Data[0] = Amount;
+    TextInBlocks[0].Data[1] = Text.size() - 2;
     std::srand(std::time(nullptr));
     for (int i = 0; i < Amount; i++) {
         if (Text.size() >= 8) {
             std::string PieceText = Text.substr(0, 8);
-            for (int j = 0; j < 8; j++)
-                if ((i != 0) || (j != 0))
+            for (int j = 0; j < 8; j++) {
+                if (!((i == 0) && ((j == 0) || (j == 1))))
                     TextInBlocks[i].Data[j] = PieceText[j];
-
+            }
         } else {
             for (int j = 0; j < Text.size(); j++)
                 TextInBlocks[i].Data[j] = Text[j];
@@ -76,15 +73,12 @@ void Decrypting(std::string &Text, const std::string &Key, const std::string &Fi
     //Creating Gamma and xoring Text with Gamma
     long long int IV = stoi(Key);
     std::srand(IV);
-    block *Gamma = new block[Amount];
     for (int i = 0; i < Amount; i++) {
+        block Gamma;
         for (int j = 0; j < 8; j++) {
-            Gamma[i].Data[j] = rand() % 128;
+            Gamma.Data[j] = rand() % 128;
         }
-    }
-
-    for (int i = 0; i < Amount; i++) {
-        TextInBlocks[i] ^ Gamma[i];
+        TextInBlocks[i] ^ Gamma;
     }
 
     int ShiftNumber = 7;
@@ -99,60 +93,35 @@ void Decrypting(std::string &Text, const std::string &Key, const std::string &Fi
     outputFile.close();
 
     delete[] TextInBlocks;
-    delete[] Gamma;
 };
 
-void Encrypting(Encrypt &DecryptedText, const std::string &Key, const std::string &FileOut) {
+void Decrypting(block *TextInBlocks, const std::string &FileOut) {
 
-    Encrypt *Current = &DecryptedText;
-    unsigned int Amount = 1;
-    int ShiftNumber = 7;
-    Current->This << ShiftNumber;
-    while (Current->next != nullptr) {
-        Current = Current->next;
-        Current->This << ShiftNumber;
-        Amount++;
-    }
-    //gamma
-    long long int IV = stoi(Key);
-    std::srand(IV);
-    block *Gamma = new block[Amount];
-    for (int i = 0; i < Amount; i++) {
-        for (int j = 0; j < 8; j++) {
-            Gamma[i].Data[j] = rand() % 128;
-        }
-    }
+    for (int i = 1; i < TextInBlocks[0].Data[0]; i++)
+        TextInBlocks[i] << 7;
+
     //xor
-    Current = &DecryptedText;
-    for (int i = 0; i < Amount; i++) {
-        Current->This ^ Gamma[i];
-        Current = Current->next;
-    }
-    Current = &DecryptedText;
-    unsigned int Size = Current->This.Data[0];
-    std::string EncryptedText;
-    for (int i = 0; i < Amount; i++) {
+    for (int i = 1; i < TextInBlocks[0].Data[0]; i++) {
+        block Gamma;
         for (int j = 0; j < 8; j++) {
-            if ((i != 0) || (j != 0))
-                EncryptedText += Current->This.Data[j];
+            Gamma.Data[j] = rand() % 128;
         }
-        Current = Current->next;
+        TextInBlocks[i] ^ Gamma;
     }
-    EncryptedText = EncryptedText.substr(0, Size);
-    Current = &DecryptedText;
+
+    unsigned int Size = TextInBlocks[0].Data[1];
+    std::string DecryptedText;
+    for (int i = 0; i < TextInBlocks[0].Data[0]; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (!((i == 0) && ((j == 0) || (j == 1))))
+                DecryptedText += TextInBlocks[i].Data[j];
+        }
+    }
+    DecryptedText = DecryptedText.substr(0, Size);
 
     std::ofstream outputFile(FileOut);
-    outputFile << EncryptedText;
+    outputFile << DecryptedText;
     outputFile.close();
-
-    Encrypt *PreCurrent;
-    for (int i = 0; i < Amount; i++) {
-        PreCurrent = Current;
-        Current = Current->next;
-        if (PreCurrent != &DecryptedText)
-            delete PreCurrent;
-
-    }
 }
 
 int main(int argc, char *argv[]) {
@@ -161,37 +130,33 @@ int main(int argc, char *argv[]) {
     std::string IV = argv[3];
     std::string FileOut = argv[4];
 
-    if (Mod == "Decrypting") {
+    if (Mod == "Encrypting") {
         std::ifstream inputFile(FileIn);
         std::string Text;
         std::getline(inputFile, Text, '\0');
         inputFile.close();
-        Text = Text;
-        Decrypting(Text, IV, FileOut);
-        std::cout << "Text was decrypted" << std::endl;
-    } else if (Mod == "Encrypting") {
+        Encrypting(Text, IV, FileOut);
+        std::cout << "Text was Encrypted" << std::endl;
+    } else if (Mod == "Decrypting") {
         std::ifstream inputFile(FileIn, std::ios::binary);
-        Encrypt DecryptedText;
-        Encrypt *Current = &DecryptedText;
-        unsigned int Size = inputFile.seekg(0, std::ios::end).tellg();
-        inputFile.close();
-        std::ifstream inputFile2(FileIn, std::ios::binary);
-        int count = 0;
-        while (count != Size) {
-            for (int i = 0; i < 8; i++) {
-                unsigned char Letter;
-                inputFile2.read(reinterpret_cast<char *>(&Letter), 1);
-                Current->This.Data[i] = Letter;
-                count++;
-            }
-            if (count != Size) {
-                Current->next = new Encrypt;
-                Current = Current->next;
-            }
-        }
-        inputFile2.close();
-        Encrypting(DecryptedText, IV, FileOut);
-        std::cout << "Text was encrypted" << std::endl;
+        block GetAmount;
+        for (int j = 0; j < 8; j++)
+            inputFile.read(reinterpret_cast<char *>(&GetAmount.Data[j]), 1);
+        GetAmount << 7;
+        long long int Key = stoi(IV);
+        std::srand(Key);
+        block FirstGamma;
+        for (int j = 0; j < 8; j++)
+            FirstGamma.Data[j] = rand() % 128;
+        GetAmount ^ FirstGamma;
+        unsigned char Amount = GetAmount.Data[0];
+        block *TextInBlocks = new block[Amount];
+        TextInBlocks[0] = GetAmount;
+        for (int i = 1; i < Amount; i++)
+            for (int j = 0; j < 8; j++)
+                inputFile.read(reinterpret_cast<char *>(&TextInBlocks[i].Data[j]), 1);
+        Decrypting(TextInBlocks, FileOut);
+        std::cout << "Text was decrypted" << std::endl;
     }
     return 0;
 }
